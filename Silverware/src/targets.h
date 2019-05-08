cdbe595ae7031c5b0d2f6e4998df49d6d2e7d590
NotFastEnuf <schrock7903@gmail.com>
2019-05-08 01:00:14 -0400

    added wolfhoo prototype target

--- Silverware/src/targets.h
+++ Silverware/src/targets.h
@@ -290,3 +290,71 @@
 #define MOTOR2_PIN_PA4
 #define MOTOR3_PIN_PA6
 #endif
+
+
+#ifdef WolfHoo_Brushless
+#define BRUSHLESS_TARGET
+//LEDS
+#define LED_NUMBER 1
+#define LED1PIN GPIO_Pin_0
+#define LED1PORT GPIOF
+#define LED2PIN GPIO_Pin_0
+#define LED2PORT GPIOF
+
+//SOFT I2C & GYRO
+#define SOFTI2C_SDAPIN GPIO_Pin_10
+#define SOFTI2C_SDAPORT GPIOA
+#define SOFTI2C_SCLPIN GPIO_Pin_9
+#define SOFTI2C_SCLPORT GPIOA
+#define SOFTI2C_GYRO_ADDRESS 0x68
+//#define SOFTI2C_GYRO_ADDRESS 0x69
+#define GYRO_ID_1 0x68
+#define GYRO_ID_2 0x98 // new id
+#define GYRO_ID_3 0x7D
+#define GYRO_ID_4 0x72
+#define SENSOR_ROTATE_180
+
+// SPI PINS DEFINITONS & RADIO
+#if defined(RX_SBUS) || defined(RX_DSMX_2048) || defined(RX_DSM2_1024) || defined(RX_CRSF) 
+#define SERIAL_RX_SPEKBIND_BINDTOOL_PIN GPIO_Pin_2
+#define SERIAL_RX_SPEKBIND_RX_PIN GPIO_Pin_3
+#define SERIAL_RX_PIN GPIO_Pin_3
+#define SERIAL_RX_PORT GPIOA
+#define SERIAL_RX_SOURCE GPIO_PinSource3
+#define SERIAL_RX_CHANNEL GPIO_AF_1
+#define SOFTSPI_NONE
+#else
+#define SOFTSPI_4WIRE
+#define SPI_MISO_PIN GPIO_Pin_0
+#define SPI_MISO_PORT GPIOA
+#define SPI_MOSI_PIN GPIO_Pin_1
+#define SPI_MOSI_PORT GPIOA
+#define SPI_CLK_PIN GPIO_Pin_2
+#define SPI_CLK_PORT GPIOA
+#define SPI_SS_PIN GPIO_Pin_3
+#define SPI_SS_PORT GPIOA
+#define RADIO_CHECK
+#define RADIO_XN297
+#endif
+
+//VOLTAGE DIVIDER
+#define BATTERYPIN GPIO_Pin_5
+#define BATTERYPORT GPIOA
+#define BATTERY_ADC_CHANNEL ADC_Channel_5
+//#define ADC_SCALEFACTOR 0.002423  //old value
+#ifndef VOLTAGE_DIVIDER_R1
+#define VOLTAGE_DIVIDER_R1 15000
+#endif
+#ifndef VOLTAGE_DIVIDER_R2
+#define VOLTAGE_DIVIDER_R2 2200
+#endif
+#ifndef ADC_REF_VOLTAGE
+#define ADC_REF_VOLTAGE 3.3
+#endif
+
+// MOTOR PINS
+#define MOTOR0_PIN_PA7
+#define MOTOR1_PIN_PB1
+#define MOTOR2_PIN_PA4
+#define MOTOR3_PIN_PA6
+#endif
